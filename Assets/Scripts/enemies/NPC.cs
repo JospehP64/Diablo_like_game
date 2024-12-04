d74a7498199f0b3831ef0438bf172770c52ed82a
JospehP64 <jmjakepardo@gmail.com>
2024-12-04 21:38:46 +0100

    04-12-2024
    -Se ha modificado el tamaño del collider del loght probe group (para las luces indirectas que reflejan el nivel)
    
    -Se ha añadido un código para que, al interactuar con el NPC (un nuevo NPC), este se voltee y en un debug.log diga "hola"
    
    -corregir errores relacionados con el movimiento del personaje y la interacción con el npc (al cliquear en el y después en cualquier parte del mapa, este dice "hola")
    
    -AÑADIR MÁS CAMBIOS AL JUEGO

--- Assets/Scripts/enemies/NPC.cs
+++ Assets/Scripts/enemies/NPC.cs
@@ -1,18 +1,20 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Outline colorOutline;
+    
+    [SerializeField] float RotationTime;
 
-    // Update is called once per frame
-    void Update()
+    // Start is called before the first frame update
+    
+    public void interact(Transform interacter)
     {
+        Debug.Log("hola");
         
+        transform.DOLookAt(interacter.position, RotationTime, AxisConstraint.Y);
     }
 }
--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -5,6 +5,8 @@ using UnityEngine.AI;
 
 public class player : MonoBehaviour
 {
+    [SerializeField]int interactDistance;
+    NPC RemainingNpc;
     private NavMeshAgent agent;
     Camera cam;
     // Start is called before the first frame update
@@ -17,11 +19,31 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (RemainingNpc)
+        {
+            if (agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
+            {
+                
+                agent.stoppingDistance = 0;
+                RemainingNpc.interact(this.transform);
+                RemainingNpc = null;
+
+            }
+        }
+        
+
         Ray laser = cam.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(laser, out RaycastHit hitThis))
         {
             if (Input.GetMouseButton(0))
             {
+               if( hitThis.transform.TryGetComponent(out NPC npcscript))
+                {
+                    RemainingNpc = npcscript;
+                    
+                        agent.stoppingDistance = interactDistance;
+                }
+
                 agent.SetDestination(hitThis.point);
             }
                 
