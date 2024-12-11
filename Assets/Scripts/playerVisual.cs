40c3029ebd704bbc73b2430ae6b3e282d6990014
JospehP64 <jmjakepardo@gmail.com>
2024-12-11 21:33:40 +0100

    11-12-2024
    -Se han añadido modelos y animaciones para el jugador y el npc
    
    -Se han creado, unos controladores de animaciones (uno para el jugador y otro para el npc) y se han añadido las animaciones correspondientes a cada uno
    
    -Se ha añadido un árbol de animaciones a los controladores
    
    -Se ha arreglado el movimiento del jugador corrigiendo la posición del terrain (terreno). Ahora el jugador puede caminar libremente por el escenario
    
    -Se ha añadido una cámara y un camera point (junto con una textura para la cámara y el raw image de la caja de conversación) para que se vea el rostro del npc al interactuar con el
    
    PROBLEMAS:
    -AUN SE DEBE CORREGIR EL PROBLEMA CON LA INTERACCIÓN DEL NPC
    
    -AUN SE DEBE INTRODUCIIR COMPLETAMENTE EL SISTEMA DE CONVERSACIÓN DEL NPC (NO ESTÁ TERMINADO)

---
+++ Assets/Scripts/playerVisual.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class playerVisual : MonoBehaviour
+{
+    Animator animator;
+    [SerializeField]NavMeshAgent agent;
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+
+        //coge la animacion del personaje
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //en función de mi velocidad actual, voy actualizando mi "velocity"
+        animator.SetFloat("blen_velocity", agent.velocity.magnitude / agent.speed);
+    }
+}
